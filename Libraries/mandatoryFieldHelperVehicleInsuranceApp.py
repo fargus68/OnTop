7bdf1dee603117980171302eea8c00d3e674df58
= <=>
2025-03-26 10:33:04 +0100

    msz - field hint and error checks part 1

--- Libraries/mandatoryFieldHelperVehicleInsuranceApp.py
+++ Libraries/mandatoryFieldHelperVehicleInsuranceApp.py
@@ -2,21 +2,16 @@ def simple_image_compare_Icons(filename):
     import cv2
     #import numpy as np
 
-    from PIL import Image
-    img = Image.open(filename)
+    #from PIL import Image
+    #img = Image.open(filename)
     #print(img.size)
     #print(img.size[0])
     #print(img.size[1])
-
-    # Lade die Bilder
-    imgIconMissingMandatory = cv2.imread('Resources/Images/MissingMandatoryField.png')
-    #imgIconFilledMandatory = cv2.imread('Resources/Images/FilledMandatoryField.png')
     imgIconActual = cv2.imread(filename)
-
-    # Konvertiere die Bilder in Graustufen
-    imgIconMissingMandatory_gray = cv2.cvtColor(imgIconMissingMandatory, cv2.COLOR_BGR2GRAY)
-    #imgIconFilledMandatory_gray = cv2.cvtColor(imgIconFilledMandatory, cv2.COLOR_BGR2GRAY)
     imgIconActual_gray = cv2.cvtColor(imgIconActual, cv2.COLOR_BGR2GRAY)
+
+    imgIconMissingMandatory = cv2.imread('Resources/Images/MissingMandatoryField.png')
+    imgIconMissingMandatory_gray = cv2.cvtColor(imgIconMissingMandatory, cv2.COLOR_BGR2GRAY)
 
     # Berechne den strukturellen Ähnlichkeitsindex (SSIM)
     from skimage.metrics import structural_similarity
--- Tests/102_AutomobileInsurance/102_AutomobileInsurance_002_VehicleData.robot
+++ Tests/102_AutomobileInsurance/102_AutomobileInsurance_002_VehicleData.robot
@@ -7,3 +7,6 @@ ${Flow}    floVehicleInsurance
 *** Test Cases ***
 102_AutomobileInsurance_002_VehicleData_001_MandatoryFields
     FlowExecution    ${Flow}    ${TEST NAME}
+
+102_AutomobileInsurance_002_VehicleData_002_FieldHintsAndErrors
+    FlowExecution    ${Flow}    ${TEST NAME}
